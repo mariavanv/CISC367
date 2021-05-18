1737407c0f8d943a654c116b194e4b09f6c93fff
Maria <mariavan@udel.edu>
2021-05-18 12:26:40 -0400

    Scrapy work

--- scrapydir/bunnies/bunnies/spiders/bunnies_spider.py
+++ scrapydir/bunnies/bunnies/spiders/bunnies_spider.py
@@ -1,4 +1,12 @@
 import scrapy
+from scrapy.item import Field, Item
+
+class Bunny(Item):
+    name = Field()
+    snrWeight = Field()
+    bunType = Field()
+    color = Field()
+    dist = Field()
 
 
 class BunniesSpider(scrapy.Spider):
@@ -13,6 +21,9 @@ class BunniesSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
+        #hxs = HtmlXPathSelector(response)
+        #item = Bunny()
+        #item["name"] = hxs.select
         page = response.url.split("/")[-2]
         filename = f'bunnies-{page}.html'
         with open(filename, 'wb') as f:
